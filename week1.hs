5821f1cd6a64f3c57e3794cd84301ccd3f8be99f
vasko01 <73233479+vasko01@users.noreply.github.com>
2021-03-01 15:58:42 +0200

    Update week1.hs

--- week1.hs
+++ week1.hs
@@ -6,19 +6,33 @@ main = do
     print (fibRec 6)
     print (fibIter 6)
 
-
+{-
+Задача 1. Да се дефинира функция myMin, която приема два аргумента и връща по-малкия от тях.
+-}
 myMin :: Integer -> Integer -> Integer
 myMin a b = if a < b then a else b
 
+{-
+Задача 2. Да се дефинира функцията isInside x a b, която проверява дали числото x се намира в затворения интервал [a, b].
+-}
 isInside :: Integer -> Integer -> Integer -> Bool
 isInside x a b = x >= a && x <= b
 
+{-
+Задача 3. Да се дефинира функция calcAverage, която пресмята на средно аритметичното от квадратите на 2 числа.
+-}
 calcAverage :: Double -> Double -> Double
 calcAverage a b = (a ^ 2 + b ^ 2) / 2
 
+{-
+Задача 4. Да се дефинира fibRec, която получава един аргумент n и връща n-тото число на Фибоначи, чрез рекурсивен процес. 
+-}
 fibRec :: Integer -> Integer
 fibRec n = if n <= 1 then 1 else fibRec (n - 1) + fibRec (n - 2)
 
+{-
+Задача 5. Да се дефинира fibIter, която получава един аргумент n и връща n-тото число на Фибоначи, чрез итеративен процес.
+-}
 fibIter :: Integer -> Integer
 fibIter n = fibIterHelper 1 1 n
     where
